f30aa436351d4a2a8464f887aa9dc45da9837289
Ivan H CArvajal Acevedo <ivanchoh99@gmail.com>
2023-11-05 20:10:19 -0500

    creacion e inicializacion del proyecto

---
+++ src/main/java/poli/edu/co/PoliTurno/PoliTurnoApplication.java
@@ -0,0 +1,13 @@
+package poli.edu.co.PoliTurno;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PoliTurnoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PoliTurnoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/poli/edu/co/PoliTurno/PoliTurnoApplicationTests.java
@@ -0,0 +1,13 @@
+package poli.edu.co.PoliTurno;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PoliTurnoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
